2ce5d2330de5457fe0bfd560b430dde41e5a949d
omar Alsaied <wwwomralsaied@gmaill.com>
2024-11-14 09:10:46 +0000

    SignIn bloc done

--- lib/screens/auth/views/welecome_screen.dart
+++ lib/screens/auth/views/welecome_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WelecomeScreen extends StatefulWidget {
@@ -7,15 +9,66 @@ class WelecomeScreen extends StatefulWidget {
   State<WelecomeScreen> createState() => _WelecomeScreenState();
 }
 
-class _WelecomeScreenState extends State<WelecomeScreen> {
+class _WelecomeScreenState extends State<WelecomeScreen>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-        "Fuck",
-        style: TextStyle(fontSize: 30),
-      )),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(20, -1.2),
+                child: Container(
+                  height: MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).colorScheme.tertiary),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(2.7, -1.2),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 1.3,
+                  width: MediaQuery.of(context).size.width / 1.3,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+              BackdropFilter(filter: ImageFilter.blur(sigmaX: 100.0,sigmaY: 100.0),child: Container(),),
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height/1.8,
+                  child: Column(children: [
+                  Padding(padding: 
+                  const EdgeInsets.symmetric(horizontal: 50.0),
+                  // child: TabBar(
+                  //   // controller: tabController,
+                  //   // unselectedLabelColor: Theme.of(context).colorScheme.,
+                    
+                  //   // tabs: tabs),
+                  // )
+                )],),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
